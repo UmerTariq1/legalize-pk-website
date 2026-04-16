d8263232a531a4ca9761a29413f8439cd7c8ba1e
Umer <umer.butt@sequire.de>
2026-04-16 05:29:25 +0200

    render routing fix for explore pages

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(staticDir, "index.html"));
 });
 
+app.get(["/article", "/article/:id"], (req, res) => {
+  res.sendFile(path.join(staticDir, "article", "index.html"));
+});
+
+app.get(["/amendment", "/amendment/:n"], (req, res) => {
+  res.sendFile(path.join(staticDir, "amendment", "index.html"));
+});
+
+app.get(["/diff", "/diff/:article"], (req, res) => {
+  res.sendFile(path.join(staticDir, "diff", "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log("Server listening on port " + PORT);
 });
